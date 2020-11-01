f931134911d1a9310018dd930d94d7321b1ab5e1
Adam McAree <arm1686@g.rit.edu>
2020-11-01 13:36:10 -0500

    now domo b is done

--- server/app.js
+++ server/app.js
@@ -13,34 +13,34 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 };
 
-mongoose.connect(dbURL, mongooseOptions, (err) =>{
-    if(err){
-        console.log('Could not connect to database');
-        throw err;
-    }
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const router = require('./router.js');
 
 const app = express();
-app.use('/assets',express.static(path.resolve(`${__dirname}/../hosted`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    secret: 'Domo Arigato',
-    resave: true,
-    saveUninitialized: true,
-}))
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
@@ -48,8 +48,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-    if(err){
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
